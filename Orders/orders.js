1eef575be1ed2dde214bd0fb3c9cd79fcfca9e9b
Vishnu Vardhan <163713763+VishnuVardhan-hub@users.noreply.github.com>
2024-05-11 19:53:51 +0530

    Add files via upload

---
+++ Orders/orders.js
@@ -0,0 +1,5 @@
+var chickenPrice = 200;
+var chickenQuantity = 1;
+var totalPriceChicken = chickenQuantity * chickenPrice;
+document.getElementById("Chicken-65-Price").textContent = totalPriceChicken;
+document.getElementById("Chicken-65-Quantity").textContent = chickenQuantity;
